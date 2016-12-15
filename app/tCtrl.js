bcbfdcd4357c798acb5dfdc0bc1134794fe0682f
elex <elextraza@gmail.com>
2016-12-15 23:55:49 +0300

    доп контроллер

--- app/tCtrl.js
+++ app/tCtrl.js
@@ -1,4 +1,6 @@
 // app/routes.js
+var db = require('../db');
+
 module.exports = function(app, passport) {
 
 	// =====================================
@@ -7,7 +9,11 @@ module.exports = function(app, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/tCtrl', isLoggedIn, function(req, res) {
-        res.send('auth tCtrl');
+        res.send( req.user);
+        /*db.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
+            res.send(err, rows);
+        });*/
+
 	});
 
 
