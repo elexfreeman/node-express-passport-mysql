311b070cea93732380035967ab536e5828a69658
elex <elextraza@gmail.com>
2016-12-20 17:24:51 +0400

    angular

---
+++ public/js/app/controllers/loginCtrl.js
@@ -0,0 +1,8 @@
+
+/* Редактирование предзаказа */
+dpApp.controller('defaultCtrl',[
+    '$scope','$rootScope','$http', '$location', '$routeParams','$templateCache',
+    function($scope, $rootScope, $http, $location, $routeParams,$templateCache) {
+        $rootScope.body_class="signup-page";
+    }
+]);
