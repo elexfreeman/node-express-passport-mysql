c51bc6a9e67f68e8111f8921ee3690bf7adf0483
elex <elextraza@gmail.com>
2016-12-20 15:11:34 +0400

    css

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
 var port     = process.env.PORT || 8080;
-
+var path = require('path');
 var passport = require('passport');
 var flash    = require('connect-flash');
 
@@ -39,11 +39,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
+app.use("/", express.static(path.join(__dirname, 'public')));
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 require('./app/tCtrl.js')(app, passport);
+
 
 // launch ======================================================================
 app.listen(port);
